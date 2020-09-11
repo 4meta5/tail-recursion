97ecb6d6794a58362431bb3249c358704f4ddda7
4meta5 <asinghchrony@protonmail.com>
2020-09-10 23:41:13 -0400

    added unit tests to fac

--- rust/fac/src/lib.rs
+++ rust/fac/src/lib.rs
@@ -18,3 +18,29 @@ pub fn optimized(n: u64) -> u64 {
 pub fn fac(n: usize) -> usize {
     (1..n + 1).fold(1, |a, b| a * b)
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn fac_tests() {
+        assert_eq!(naive(5), 120);
+        assert_eq!(optimized(5), 120);
+        assert_eq!(fac(5), 120);
+        assert_eq!(naive(6), 720);
+        assert_eq!(optimized(6), 720);
+        assert_eq!(fac(6), 720);
+        assert_eq!(naive(7), 5040);
+        assert_eq!(optimized(7), 5040);
+        assert_eq!(fac(7), 5040);
+        assert_eq!(naive(8), 40320);
+        assert_eq!(optimized(8), 40320);
+        assert_eq!(fac(8), 40320);
+        assert_eq!(naive(9), 362880);
+        assert_eq!(optimized(9), 362880);
+        assert_eq!(fac(9), 362880);
+        assert_eq!(naive(10), 3628800);
+        assert_eq!(optimized(10), 3628800);
+        assert_eq!(fac(10), 3628800);
+    }
+}
